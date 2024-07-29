61c999cf1d3c9384348ce85e892d9e309cc2430b
Atmajo <39727009+Atmajo@users.noreply.github.com>
2024-07-29 17:02:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import { configDotenv } from "dotenv";
+import { v4 as uuid } from "uuid";
+configDotenv();
+
+const app = express()
+
+let otp_store = {}
+
+app.get('/', (res, req) => {
+    req.send("Welcome to Cloud OTP Service")
+})
+
+app.post('/send-otp', (res, req) => {
+    let otp = "";
+    const phone = res.query.phone;
+
+    function generateOtp() {
+        let otpString = "";
+        const vals = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+        for (let index = 0; index < 6; index++) {
+            otpString = otpString + vals[parseInt(Math.random() * 10)]
+        }
+
+        return otpString
+    }
+
+    if (phone.length === 10) {
+        otp = generateOtp();
+        otp_store = { "id": uuid(), "phone": phone, "otp": otp, "isVerified": false }
+
+        req.send(`Your OTP is ${otp_store.otp}`)
+    } else {
+        req.send("Invalid number")
+    }
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server started in http://localhost:${process.env.PORT}`)
+})
